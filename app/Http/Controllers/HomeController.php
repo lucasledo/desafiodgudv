7a61f742fba80d2b10a1bcf71af279909496a57a
Lucas Emmanuel <lucas.emmanuel28@gmail.com>
2024-10-10 11:40:42 -0300

    Added bootstrap theme

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $tasksDone      = Task::where('status', 1)->where('user_id', auth()->user()->id)->count();
+        $tasksNotDone   = Task::where('status', 0)->where('user_id', auth()->user()->id)->count();
+
+        return view('home', compact('tasksDone', 'tasksNotDone'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,16 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
     Route::resource('tasks', TaskController::class);
 });
 
-require __DIR__.'/auth.php';
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
