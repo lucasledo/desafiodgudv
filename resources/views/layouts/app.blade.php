24d9e596a34304200f8965648f3f53cc7fffde78
Lucas Emmanuel <lucas.emmanuel28@gmail.com>
2024-10-11 15:44:00 -0300

    improved code, added files and readme

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,12 +33,14 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link @if(request()->routeIs('home')) active @endif" aria-current="page" href="{{url('/')}}">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link @if(request()->routeIs('tasks.index')) active @endif" href="{{route('tasks.index')}}">Tarefas</a>
-                        </li>
+                        @auth
+                            <li class="nav-item">
+                                <a class="nav-link @if(request()->routeIs('home')) active @endif" aria-current="page" href="{{url('/')}}">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link @if(request()->routeIs('tasks.index')) active @endif" href="{{route('tasks.index')}}">Tarefas</a>
+                            </li>
+                        @endauth
                     </ul>
 
                     <!-- Right Side Of Navbar -->
--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -9,6 +9,7 @@
                     <div class="d-flex justify-content-between align-items-center flex-column flex-sm-row">
                         {{ __('Tasks') }}
                         <div>
+                            <a href="{{route('tasks.index')}}" type="button" class="btn btn-secondary">{{ __('All Tasks')}}</a>
                             <a href="{{route('tasks.index') . '?status=1'}}" type="button" class="btn btn-success">{{ __('Tasks Done')}}</a>
                             <a href="{{route('tasks.index') . '?status=0'}}" type="button" class="btn btn-warning">{{ __('Tasks Not Done')}}</a>
                             <button type="button" data-bs-toggle="modal" data-bs-target="#addTask" class="btn btn-primary">{{ __('New Task') }}</button>
